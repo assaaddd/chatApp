902fdc9b31947a83ed12f9b66fe5b055924e914c
sdbty <asadbeeksafaraliyev@gmail.com>
2024-04-13 01:45:58 +0500

    last part deploying ended

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
@@ -11,6 +12,9 @@ import userRoutes from './routes/user.route.js';
 
 const PORT = process.env.PORT || 4000;
 
+//to deploy
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -19,10 +23,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+//to deploy
+app.use(express.static(path.join(__dirname , "/frontend/dist")));
 
-// app.get('/' , (req , res) => {
-//     res.send('Hello world!')
-// })
+app.get("*" , (req , res) => {
+    res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"))
+})
+
 
 
 server.listen(PORT, () => {
